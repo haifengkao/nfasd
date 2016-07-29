e8ff2c12f0cce56e22c1a66ad8ce5cc26d4cb85f
Hai Feng Kao <haifeng@cocoaspice.in>
2016-07-29 23:38:06 +0800

    - matching algorithm done

--- bin/dist.py
+++ bin/dist.py
@@ -1,24 +1,96 @@
 #!/usr/bin/env python
-import os     # to execute nvim
+
+def index_map(user_input, string):
+  indices = {}
+  for ch in user_input:
+    if not indices.has_key(ch):
+      indices[ch] = []
+
+  for i, ch in enumerate(string):
+    if indices.has_key(ch):
+      index = indices[ch]
+      index.append(i)
+
+  return indices
 
 # for file name partial matching
 def dist(user_input, string):
-  if len(user_input) > len(string):
-    # impossible matching, return highest possible cost
-    return len(user_input)
+  if len(user_input) <= 0:
+    # are you kidding me?
+    return 0
 
-    distances = range(len(user_input) + 1)
-    for i2, c2 in enumerate(string):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(user_input):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
+  max_cost = len(user_input) + 1
+  indices = index_map(user_input, string)
+
+  # first line
+  distances = []
+  for i in indices[user_input[0]]:
+    distances.append((i, 0)) # index, cost
+
+  for ch in user_input[1:]:
+    if len(distances) <= 0:
+      break
+    distances_ = []
+    for i in indices[ch]:
+      min_cost = max_cost
+      for t in distances:
+        # TODO: binary search?
+        if (t[0] >= i):
+          continue
+
+        if t[0] != i - 1:
+          # non-consecutive matching
+          # cost++
+          # e.g. compare 'aa' against 'aba'
+          cost = t[1] + 1
+        else:
+          # consecutive matching
+          # e.g. compare 'aa' against 'aa' or 'baa' or 'aab'
+          cost = t[1]
+        if cost < min_cost:
+          min_cost = cost
+
+      distances_.append((i, min_cost))
+    distances = distances_
+
+  if len(distances) > 0:
     return distances[-1]
+  else:
+    return None # user_input is not a subsequence of string
 
 
 if __name__ == "__main__":
-  print dist('aa', 'abba')
+  print dist('aa', 'aa')                  # =>(1, 0)
+  print dist('aa', 'aab')                 # =>(1, 0)
+  print dist('aa', 'baa')                 # =>(2, 0)
+  print dist('aa', 'bbaa')                # =>(3, 0)
+  print dist('aa', 'aabb')                # =>(1, 0)
+  print dist('aa', 'abba')                # =>(3, 1)
+  print dist('aaa', 'ababa')              # =>(4, 2)
+  print dist('init', 'ing/install.sh')    # =>(7, 2)
+  print dist('init', 'ing/nvim/init.vim') # =>(12, 0)
+  print dist('init', 'ing/insitall.sh')   # =>(8, 1)
+  print dist("foo",  "foobar")            # =>(2, 0)
+  print dist("foo",  "FooBar")            # =>None
+  print dist("fb",  "foobar")             # =>(3, 1)
+  print dist("",  "foo bar")              # =>0
+  print dist("",  "")                     # =>0
+  print dist("f",  "")                    # =>None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
