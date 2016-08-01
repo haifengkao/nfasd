e71a039aad75d7f6b269fb73eff70b39ba81ba0d
Hai Feng Kao <haifeng@cocoaspice.in>
2016-08-01 17:11:40 +0800

    - menu complete done

--- bin/bxrun.sh
+++ bin/bxrun.sh
@@ -9,12 +9,18 @@ _bxrun() {
     COMPREPLY=( $( compgen -W '$( _bxrun_lst )' -- $cur  ) )
 }
 _bxrun_zsh() {
+    compstate[insert]=menu # no expand
     compadd -U `_bxrun_lst`
 }
+
+# copied from fasd
+#[ "$ZSH_VERSION" ] && emulate sh && setopt localoptions
+#setopt MENU_COMPLETE
+#setopt  NO_LIST_BEEP
 if type compdef >/dev/null 2>/dev/null; then
     # zsh
     compdef _bxrun_zsh bxrun
-else if type complete >/dev/null 2>/dev/null; then
-    # bash
-    complete -F _bxrun bxrun
+#else if type complete >/dev/null 2>/dev/null; then
+    ## bash
+    #complete -F _bxrun bxrun
 fi;
