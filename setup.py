b890abe38bfb3362587fbe779a40e28b2f40148b
Hai Feng Kao <haifeng@cocoaspice.in>
2019-07-13 05:40:20 +0800

    0.18 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nfasd',
-      version='0.17',
+      version='0.18',
       description='fasd for neovim',
       keywords='fasd neovim nvim nyaovim jumplist',
       url='http://github.com/haifengkao/nfasd',
